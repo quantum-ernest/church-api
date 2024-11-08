72c6afdd4cb4f1333adad327f94eec4ea0c8f085
quantum-ernest <easare@quantumgroupgh.com>
2024-11-08 10:04:05 +0000

    add data schemas

---
+++ schemas/__init__.py
@@ -0,0 +1,13 @@
+from schemas.member import MemberSchemaIn, MemberSchemaOut, MemberSchemaUpdate
+from schemas.user import UserSchemaIn, UserSchemaOut, UserSchemaUpdate
+from schemas.auth import (ChangePasswordSchemaIn, ChangePasswordSchemaOut,
+                          LoginSchemaIn, LoginSchemaOut,
+                          OtpEmailGenerateSchemaIn, OtpEmailLoginSchemaIn,
+                          OtpGenerateSchemaOut)
+from schemas.contribution import (ContributionSchemaIn, ContributionSchemaOut,
+                                  ContributionSchemaUpdate,
+                                  ContributionTypeSchemaIn,
+                                  ContributionTypeSchemaOut,
+                                  ContributionTypeSchemaUpdate)
+from schemas.department import (DepartmentSchemaIn, DepartmentSchemaOut,
+                                DepartmentSchemaUpdate)
